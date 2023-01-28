953ac68861863aac377fd66c1d8cc1db1e8ffb9a
zazikant <zazikant@gmail.com>
2023-01-28 11:31:05 +0530

    second

--- tutorial/tutorial/spiders/test.py
+++ tutorial/tutorial/spiders/test.py
@@ -3,7 +3,7 @@ import scrapy
 
 class TestSpider(scrapy.Spider):
     name = 'test'
-    allowed_domains = ['x']
+    allowed_domains = ['s']
     start_urls = ['http://google.com/']
 
     def parse(self, response):
